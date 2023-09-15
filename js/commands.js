90a5c3183b8f8deaa2df26d67ce583c557ab1e52
JohnCouillo <john.dugland.celeste@gmail.com>
2023-09-15 19:32:49 +0200

    first commit

---
+++ js/commands.js
@@ -0,0 +1,39 @@
+var mastodon = "https://mastodon.social/@ratel";
+
+about = [
+  "<br>",
+  "<span class='neutral'>Hi, my name is John Berlingot, which is not my real name but we're supposed to have a name to communicate.</span>",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'mastodon            <a href="' +
+    mastodon +
+    '" target="blank">mastodon.social/@ratel' +
+    "</a>",
+  "facebook            <span class='neutral'>Never again</span>",
+  "MySpace             <span class='neutral'>huhu</span>",
+  '<span class="command">IRC</span>                 <span class="neutral">Now this is getting interesting</span>',
+  "<br>",
+];
+
+irc = [
+  "<br>",
+  "<span class='neutral'>And now for something completely different.</span>",
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>            <span class="neutral">Displays who I am</span>',
+  '<span class="command">clear</span>            <span class="neutral">Clears the terminal</span>',
+  '<span class="command">email</span>            <span class="neutral">Don\'t even try.</span>',
+  '<span class="command">social</span>           <span class="neutral">My social networks (and there\'s plenty of them!)</span>',
+  "<br>",
+];
+
+banner = [
+  `<div class="pt-2"><span class="neutral">(if it's white, you can type it in).</span></div>`,
+];
+ 
---
+++ js/main.js
@@ -0,0 +1,187 @@
+/* let's stay focused! */
+/* As long as your mouse points to mother, everything's fine, you can type in whatever you want, it will be listened to!*/
+
+const app = document.querySelector("#mother");
+app.addEventListener("click", function(event){
+  const input = document.querySelector("input");
+  input.focus();
+})
+
+
+/* the main part */
+
+var before = document.getElementById("before");
+var liner = document.getElementById("liner");
+var command = document.getElementById("typer");
+var textarea = document.getElementById("texter");
+var terminal = document.getElementById("terminal");
+
+var git = 0;
+var pw = false;
+var commands = [];
+
+setTimeout(function () {
+  loopLines(banner, "", 80);
+  textarea.focus();
+}, 100);
+
+window.addEventListener("keyup", enterKey);
+
+
+//init
+textarea.value = "";
+command.innerHTML = textarea.value;
+
+function enterKey(e) {
+  if (e.keyCode == 181) {
+    document.location.reload(true);
+  }
+ 
+    if (e.keyCode == 13) {
+      commands.push(command.innerHTML);
+      git = commands.length;
+      addLine(
+        "john@berlingot:~$ " + command.innerHTML,
+        "no-animation",
+        0
+      );
+      commander(command.innerHTML.toLowerCase());
+      command.innerHTML = "";
+      textarea.value = "";
+    }
+    if (e.keyCode == 38 && git != 0) {
+      git -= 1;
+      textarea.value = commands[git];
+      command.innerHTML = textarea.value;
+    }
+    if (e.keyCode == 40 && git != commands.length) {
+      git += 1;
+      if (commands[git] === undefined) {
+        textarea.value = "";
+      } else {
+        textarea.value = commands[git];
+      }
+      command.innerHTML = textarea.value;
+    }
+  }
+
+
+function commander(cmd) {
+  switch (cmd.toLowerCase()) {
+    case "help":
+      loopLines(help, "highlighted margin", 80);
+      break;
+    case "about":
+      loopLines(about, "highlighted margin", 80);
+      break;
+      case "irc":
+      loopLines(irc, "highlighted margin", 80);
+      break;
+    case "social":
+      loopLines(social, "highlighted margin", 80);
+      break;
+    case "history":
+      addLine("<br>", "", 0);
+      loopLines(commands, "highlighted", 80);
+      addLine("<br>", "command", 80 * commands.length + 50);
+      break;
+      case "mastodon":
+      addLine("Opening mastodon in a new tab...", "highlighted", 80);
+      newTab(mastodon);
+      break;
+    case "clear":
+      setTimeout(function () {
+        terminal.innerHTML = 
+          '<a id="before"><div><span class="neutral">Type <span class="command">help</span> for a list of supported commands.</span></div></a>';
+         before = document.getElementById("before");
+        
+      }, 1);
+      break;
+      
+    case "banner":
+      loopLines(banner, "", 80);
+      break;
+      
+    default:
+      addLine(
+        '<span class="error">There\'s no such command.</span>',
+        "error",
+        120
+      );
+      break;
+  }
+}
+
+function newTab(link) {
+  setTimeout(function () {
+    window.open(link, "_blank");
+  }, 500);
+}
+
+function addLine(text, style, time) {
+  var t = "";
+  for (let i = 0; i < text.length; i++) {
+    if (text.charAt(i) == " " && text.charAt(i + 1) == " ") {
+      t += "&nbsp;&nbsp;";
+      i++;
+    } else {
+      t += text.charAt(i);
+    }
+  }
+  setTimeout(function () {
+    var next = document.createElement("p");
+    next.innerHTML = t;
+    next.className = style;
+
+    before.parentNode.insertBefore(next, before);
+
+    window.scrollTo(0, document.body.offsetHeight);
+  }, time);
+}
+
+function loopLines(name, style, time) {
+  name.forEach(function (item, index) {
+    addLine(item, style, index * time);
+  });
+}
+
+/* the caret.js part */
+
+function $(elid) {
+    return document.getElementById(elid);
+  }
+  
+  var cursor;
+  window.onload = init;
+  
+  function init() {
+    cursor = $("cursor");
+    cursor.style.left = "0px";
+  }
+  
+  function nl2br(txt) {
+    return txt.replace(/\n/g, "");
+  }
+  
+  function typeIt(from, e) {
+    e = e || window.event;
+    var w = $("typer");
+    var tw = from.value;
+    if (!pw) {
+      w.innerHTML = nl2br(tw);
+    }
+  }
+  
+  function moveIt(count, e) {
+    e = e || window.event;
+    var keycode = e.keyCode || e.which;
+    if (keycode == 37 && parseInt(cursor.style.left) >= 0 - (count - 1) * 10) {
+      cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
+    } else if (keycode == 39 && parseInt(cursor.style.left) + 10 <= 0) {
+      cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
+    }
+  }
+  
+  function alert(txt) {
+    console.log(txt);
+  }
