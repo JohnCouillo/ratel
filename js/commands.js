c549238fd45b3ad8f52b3565133d28d1a485e168
JohnCouillo <john.dugland.celeste@gmail.com>
2023-09-15 21:34:52 +0200

    some additions

--- js/commands.js
+++ js/commands.js
@@ -1,4 +1,5 @@
 var mastodon = "https://mastodon.social/@ratel";
+var irc_tuto = "https://www.youtube.com/watch?v=-4ab5-F72nY";
 
 about = [
   "<br>",
@@ -13,14 +14,20 @@ social = [
     '" target="blank">mastodon.social/@ratel' +
     "</a>",
   "facebook            <span class='neutral'>Never again</span>",
-  "MySpace             <span class='neutral'>huhu</span>",
-  '<span class="command">IRC</span>                 <span class="neutral">Now this is getting interesting</span>',
+  "myspace             <span class='neutral'>huhu</span>",
+  '<span class="command">irc</span>                 <span class="neutral">Now this is getting interesting</span>',
   "<br>",
 ];
 
 irc = [
   "<br>",
-  "<span class='neutral'>And now for something completely different.</span>",
+  "<span class='neutral'>IRC (Internet Relay Chat) is by far my favorite social network in that it's not what most people would call a social network. It's the relic of a time when what one was thinking and saying mattered more than who their 'friends' were and how popular they looked.</span>",
+  "<br>",
+  "<span class='neutral'>There simply wasn't any profile to keep the record of this kind of things. You had to f***ing LISTEN to what they were saying!</span>",
+  "<br>",
+  "<span class='neutral'>And in this regard, I doubt 'modern' social networks are the best option.</span>",
+  "<br>",
+  "<span class='neutral'>In case you're interested, <a href='https://www.youtube.com/watch?v=-4ab5-F72nY' target='_blank'>here is a very short intro.</a></span>",
   "<br>",
 ];
 
@@ -30,6 +37,8 @@ help = [
   '<span class="command">clear</span>            <span class="neutral">Clears the terminal</span>',
   '<span class="command">email</span>            <span class="neutral">Don\'t even try.</span>',
   '<span class="command">social</span>           <span class="neutral">My social networks (and there\'s plenty of them!)</span>',
+  "<br>",
+  "<span class='neutral'>But it won't help much because most commands are (loosely) kept secret. </span>",
   "<br>",
 ];
 
